10772df2a0a148f51a7665b50ef28ffa6d637656
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2021-09-22 09:25:38 -0400

    Add time & memory measurements to run-all.sh in alternating-bit-variants

--- alternating-bit-variants/run-all.sh
+++ alternating-bit-variants/run-all.sh
@@ -1,9 +1,32 @@
 #! /bin/bash
 
+# TODO: Allow this value to be changed from the command line.
+max_compute_time_sec=300
+
+# OS detection
+function is_osx() {
+  [[ "$OSTYPE" =~ ^darwin ]] || return 1
+}
+function is_ubuntu() {
+  [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
+}
+
+if is_osx; then
+    TIME_CMD="/usr/bin/time -lp"
+fi
+if is_ubuntu; then
+    TIME_CMD="/usr/bin/time --verbose"
+fi
+
 TLC="java -XX:+IgnoreUnrecognizedVMOptions -XX:+UseParallelGC -cp $TLA2TOOLS_DIR/tla2tools.jar tlc2.TLC"
 
 for j in `seq 1 16`
 do
+    # I do not want to bother giving an estimated compute time for all
+    # of the TLC runs that are under 30 seconds, only for the longer
+    # ones.  Treat all 'short' ones as 1 second, even if that is quite
+    # far off numerically.
+    estimated_compute_sec=1
     case $j in
 	1) expected_status=0
 	   ARGS="AB_ql3.tla -config AB_ql3_safety_only.cfg"
@@ -54,14 +77,24 @@ do
 	   ARGS="-difftrace ABQ_nonfifo_ql.tla -config AB_ql3_safety_only.cfg"
 	   ;;
     esac
-    ${TLC} ${ARGS} >& out-$j.txt
-    exit_status=$?
     echo ""
-    echo "$j \$TLC ${ARGS}"
-    echo "exit_status: ${exit_status}"
-    echo "expected_status: ${expected_status}"
-    if [ $exit_status != $expected_status ]
+    if [ $estimated_compute_sec -gt $max_compute_time_sec ]
     then
-	echo "FIXME"
+	skip=1
+	echo "Skipping the following command because estimated compute time $estimated_compute_sec sec > $max_compute_time_sec sec"
+    else
+	skip=0
+	${TIME_CMD} ${TLC} ${ARGS} >& out-$j.txt
+	exit_status=$?
+    fi
+    echo "$j $TIME_CMD \$TLC ${ARGS}"
+    echo "expected_status: ${expected_status}"
+    if [ $skip -eq 0 ]
+    then
+	echo "exit_status: ${exit_status}"
+	if [ $exit_status != $expected_status ]
+	then
+	    echo "FIXME"
+	fi
     fi
 done
